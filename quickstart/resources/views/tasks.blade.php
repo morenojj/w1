59b298e35e252a93c8ce363dff741d2ff080a79a
moreno <oneromjoaquim@gmail.com>
2019-02-28 18:19:48 +0200

    first commit

--- quickstart/resources/views/tasks.blade.php
+++ quickstart/resources/views/tasks.blade.php
@@ -35,4 +35,69 @@
     </div>
 
     <!-- TODO: Current Tasks -->
-@endsection
+<!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                       <tr>
+    <!-- Task Name -->
+    <td class="table-text">
+        <div>{{ $task->name }}</div>
+    </td>
+					
+    <!-- Delete Button -->
+    <td>
+        <form action="/task/{{ $task->id }}" method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+
+            <button>Delete Task</button>
+        </form>
+    </td>
+</tr>
+                            <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: Delete Button -->
+                                </td>
+                            </tr>
+                       <tr>
+    <!-- Task Name -->
+    <td class="table-text">
+        <div>{{ $task->name }}</div>
+    </td>
+					
+    <!-- Delete Button -->
+    <td>
+        <form action="/task/{{ $task->id }}" method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+
+            <button>Delete Task</button>
+			<input type="hidden" name="_method" value="DELETE">
+        </form>
+    </td>
+</tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
